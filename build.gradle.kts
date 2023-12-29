1ac1dd46796890a74020b1382efaaeee3a5c56ad
MadalinaPuiu <madlenPuiu@gmail.com>
2023-12-29 16:04:11 +0200

    setup gradle, manifest, and resources

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,3 +3,17 @@ plugins {
     id("com.android.application") version "8.1.1" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
 }
+
+buildscript {
+    repositories{
+        google()
+        jcenter()
+        mavenCentral()
+    }
+    dependencies{
+        classpath("com.android.tools.build:gradle:4.0.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.10")
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.5.0")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.28-alpha")
+    }
+}
